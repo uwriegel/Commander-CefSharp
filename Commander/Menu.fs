eaae1f8aa9f1062f58997502557f8a66bb9e5f5b
uriegel <uriegel@hotmail.de>
2019-02-13 19:16:45 +0100

    Started FsTools

--- Commander/Menu.fs
+++ Commander/Menu.fs
@@ -8,17 +8,8 @@ open Browser
 open System.Collections
 open System.Collections.Generic
 open System.Windows.Forms
+open EnumerableExtensions   
 
-let castEnumerator<'U> (enumerator: IEnumerator) = {
-    new IEnumerator<'U> with
-        member x.Current with get() = enumerator.Current :?> 'U
-    interface IEnumerator with
-        member x.Current with get() = enumerator.Current
-        member x.MoveNext() = enumerator.MoveNext()
-        member x.Reset() = enumerator.Reset()
-    interface IDisposable with
-        member x.Dispose() = ()
-}      
 
 let makeSeq enumerator = {
     new IEnumerable<'U> with
---
+++ FsTools/EnumerableExtensions.fs
@@ -0,0 +1,18 @@
+﻿module EnumerableExtensions
+
+open System
+open System.Collections
+open System.Collections.Generic
+
+let castEnumerator<'U> (enumerator: IEnumerator) = {
+    new IEnumerator<'U> with
+        member x.Current with get() = enumerator.Current :?> 'U
+    interface IEnumerator with
+        member x.Current with get() = enumerator.Current
+        member x.MoveNext() = enumerator.MoveNext()
+        member x.Reset() = enumerator.Reset()
+    interface IDisposable with
+        member x.Dispose() = ()
+}      
+
+
